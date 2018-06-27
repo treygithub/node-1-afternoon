9bc8738c64a720ee92cb8006546de1e260d23866
Root <robhunn74@gmail.com>
2018-06-27 17:08:16 -0500

    bug fix 2

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const bodyParser = require(`body-parser`)
+const express = require('express');
+const bodyParser = require('body-parser');
 const mc = require( __dirname + '/controllers/messages_controller');
 
 const app = express();
-app.use( bodyParser.json() );
 
+app.use( bodyParser.json() );
+app.use( express.static( __dirname + '/../public/build' ) );
+
+const baseURL = "/api/messages";
+app.post( baseURL, mc.create );
+app.get( baseURL, mc.read );
+app.put( `${baseURL}/:id`, mc.update );
+app.delete( `${baseURL}/:id`, mc.delete );
 
 const port = 3000;
-
-const messagesBaseUrl = "/api/messages";
-app.post( messagesBaseUrl, mc.create );
-app.get( messagesBaseUrl, mc.read );
-app.put( `${messagesBaseUrl}/:id`, mc.update );
-app.delete( `${messagesBaseUrl}/:id`, mc.delete );
-
-app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
+app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
